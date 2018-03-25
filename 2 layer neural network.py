130bf48f7b809ffaa3728d9bc8a3ab062ae14246
waseef3277 <36242340+waseef3277@users.noreply.github.com>
2018-03-26 00:08:39 +0500

    Add files via upload

---
+++ 2 layer neural network.py
@@ -0,0 +1,115 @@
+from sklearn.datasets import make_moons
+from matplotlib import pyplot
+from pandas import DataFrame
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import sklearn
+
+# generate 2d classification dataset
+X, y = make_moons(n_samples=200, noise=0.1)
+# scatter plot, dots colored by class value
+df = DataFrame(dict(x=X[:, 0], y=X[:, 1], label=y))
+'''colors = {0: 'red', 1: 'blue'}
+fig, ax = pyplot.subplots()
+grouped = df.groupby('label')
+for key, group in grouped:
+    group.plot(ax=ax, kind='scatter', x='x', y='y', label=key, color=colors[key])
+pyplot.show()'''
+
+num_examples = len(X)  # training set size
+nn_input_dim = 2  # input layer dimensionality
+nn_output_dim = 2  # output layer dimensionality
+
+# Gradient descent parameters (I picked these by hand)
+alpha = 0.01  # learning rate for gradient descent
+reg_lambda = 0.01  # regularization strength
+
+
+# Helper function to evaluate the total loss on the dataset
+def calculate_loss(model):
+    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
+    # Forward propagation to calculate our predictions
+    z1 = X.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    exp_scores = np.exp(z2)
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    # Calculating the loss
+    corect_logprobs = -np.log(probs[range(num_examples), y])
+    data_loss = np.sum(corect_logprobs)
+    # Add regularization term to loss
+    data_loss += reg_lambda / 2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
+    return 1./num_examples * data_loss
+
+
+# Helper function to predict an output (0 or 1)
+def predict(model, x):
+    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
+    # Forward propagation
+    z1 = x.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    exp_scores = np.exp(z2)
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    return np.argmax(probs, axis=1)
+
+
+# This function learns parameters for the neural network and returns the model.
+# - nn_hdim: Number of nodes in the hidden layer
+# - num_passes: Number of passes through the training data for gradient descent
+# - print_loss: If True, print the loss every 1000 iterations
+
+def Neural_network(nn_hdim, num_passes=1501, print_loss=False):
+    # Initialize the parameters to random values. We need to learn these.
+    np.random.seed(0)
+    W1 = np.random.randn(nn_input_dim, nn_hdim) / np.sqrt(nn_input_dim)
+    b1 = np.zeros((1, nn_hdim))
+    W2 = np.random.randn(nn_hdim, nn_output_dim) / np.sqrt(nn_hdim)
+    b2 = np.zeros((1, nn_output_dim))
+
+    # This is what we return at the end
+    model = {}
+
+    # Gradient descent. For each batch...
+    for i in range(0, num_passes):
+
+        # Forward propagation
+        z1 = X.dot(W1) + b1
+        a1 = np.tanh(z1)
+        z2 = a1.dot(W2) + b2
+        exp_scores = np.exp(z2)
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+
+        # Backpropagation
+        delta3 = probs
+        delta3[range(num_examples), y] -= 1
+        dW2 = (a1.T).dot(delta3)
+        db2 = np.sum(delta3, axis=0, keepdims=True)
+        delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2))
+        dW1 = np.dot(X.T, delta2)
+        db1 = np.sum(delta2, axis=0)
+
+        # Add regularization terms (b1 and b2 don't have regularization terms)
+        dW2 += reg_lambda * W2
+        dW1 += reg_lambda * W1
+
+        # Gradient descent parameter update
+        W1 -= alpha * dW1
+        b1 -= alpha * db1
+        W2 -= alpha * dW2
+        b2 -= alpha * db2
+
+        # Assign new parameters to the model
+        model = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+
+        print("Loss after iteration %i: %f" % (i, calculate_loss(model)))
+
+    return model
+
+
+# Build a model with a 3-dimensional hidden layer
+model = Neural_network(3, print_loss=True)
+predict(model, X)
+
+print("Final Loss: %f" % (calculate_loss(model)))
